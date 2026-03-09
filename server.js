e0f1620d7923903bf3619e8db0744f362a329c62
Sergei Zaimovskii <zaims@mail.broward.edu>
2026-03-10 00:16:24 +0300

    📝 docs(command): Запуск Node сервера в контейнере без Dockerfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = 8888;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Node server");
+  res.end("Hello from Docker container");
 });
 
 server.listen(PORT, () => {
